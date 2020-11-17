4e5419bb3500fa5964790a03d8695015b2a1b375
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-11-17 14:14:08 -0300

    adds button to home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,13 +4,32 @@ import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 const Home: React.FC = () => (
   <Container>
     <Image source={logoImg} width={256} />
 
-    <Title>Consulte suas correspondências.</Title>
+    <Title>Consulte suas correspondências</Title>
+    <Button>Login</Button>
+    <Button
+      isWhite
+      style={{
+        shadowColor: 'rgba(78, 79, 114, 0.08)',
+        shadowOffset: {
+          width: 16,
+          height: 29,
+        },
+        shadowOpacity: 0.01,
+        shadowRadius: 60,
+
+        elevation: 8,
+      }}
+    >
+      Cadastre-se
+    </Button>
   </Container>
 );
 
