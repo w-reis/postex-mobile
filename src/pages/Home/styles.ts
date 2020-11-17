9efd5d10f30cb6dd92706a1df85150f7c3938923
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-11-17 14:14:42 -0300

    style: fixes layout

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,11 +4,12 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
   font-size: 20px;
   color: #2c2929;
   font-family: 'Roboto-Medium';
-  margin: 50px 0 0 0;
+  margin: 0px 0 100px 0;
 `;
