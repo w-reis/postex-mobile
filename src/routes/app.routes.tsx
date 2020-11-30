29549c47399dd298686e773b1ed36cad060dce9a
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-11-30 14:14:28 -0300

    creates drawer navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Dashboard from '../pages/Dashboard';
 
-const App = createStackNavigator();
+const App = createDrawerNavigator();
 
 const AppRoutes: React.FC = () => {
   return (
-    <App.Navigator
-      screenOptions={{
-        headerShown: false,
-        cardStyle: { backgroundColor: '#FAFBFD' },
-      }}
-    >
+    <App.Navigator>
       <App.Screen name="Dashboard" component={Dashboard} />
     </App.Navigator>
   );
