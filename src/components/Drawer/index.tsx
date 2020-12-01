444d88a0c758bd8f7ccd3beb8068d4c8f4cc52c8
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-12-01 19:21:32 -0300

    feature: delete account

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-import { Image } from 'react-native';
+import { Image, Alert } from 'react-native';
 
 import {
   DrawerContentComponentProps,
@@ -16,9 +16,43 @@ import { useAuth } from '../../hooks/auth';
 import logo from '../../assets/logo.png';
 
 import { Header, Title } from './styles';
+import api from '../../services/api';
 
 const Drawer: React.FC<DrawerContentComponentProps> = (props) => {
-  const { signOut } = useAuth();
+  const { signOut, recipient, token } = useAuth();
+
+  const deleteAcount = useCallback(async () => {
+    Alert.alert(
+      'Tem certeza?',
+      'Ao deletar sua conta, todas as correspondências vinculadas a ela serão perdidas.',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          style: 'default',
+          onPress: async () => {
+            try {
+              await api.delete(`/recipients/?id=${recipient.id}`, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+              signOut();
+            } catch (error) {
+              Alert.alert(
+                'Erro ao deletar',
+                'Ocorreu um erro ao deletar a conta.',
+              );
+            }
+          },
+        },
+      ],
+    );
+  }, [recipient.id, token, signOut]);
+
   return (
     <DrawerContentScrollView {...props}>
       <Header>
@@ -36,6 +70,18 @@ const Drawer: React.FC<DrawerContentComponentProps> = (props) => {
         onPress={signOut}
         icon={() => <Icon name="sign-out" size={24} color="#0748b4" />}
         labelStyle={{ fontSize: 18, fontFamily: 'Roboto-Medium' }}
+      />
+      <DrawerItem
+        label="Deletar conta"
+        onPress={deleteAcount}
+        icon={() => (
+          <Icon name="exclamation-circle" size={24} color="#c53030" />
+        )}
+        labelStyle={{
+          fontSize: 18,
+          fontFamily: 'Roboto-Medium',
+          color: '#c53030',
+        }}
       />
     </DrawerContentScrollView>
   );
